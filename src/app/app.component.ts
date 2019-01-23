ebb766de0e1cddd89491fea6a9b546ec1b635c6d
Alexander <rednaxe7@gmail.com>
2019-01-22 23:10:02 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  nombre:string = 'Alexander';
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9,10];
+  PI:number = Math.PI;
+  n:number = 0.234;
+  salario:number = 1234.5;
+  
+  persona:Object = {
+   nombre: 'Alexander',
+   apellido: 'Piñero',
+   edad: 33,
+   direccion: {
+     calle: "Sucre con Anzoategui",
+     edif: "Res. Belen Blanco",
+     piso: 7,
+     apto: "7-A"
+   }
+  };
+
+  valorDePromesa = new Promise((resolve,reject) => {
+    setTimeout(()=>resolve('llego la data!'),3500);
+  });
+
+  fecha = new Date();
 }
